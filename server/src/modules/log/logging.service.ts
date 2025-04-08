e5600c3b90f3a981095bee4575b94a8a412ac6cf
nazmul53p <nazmul2018s@gmail.com>
2025-04-08 15:50:54 +0600

    ongoing

--- server/src/modules/log/logging.service.ts
+++ server/src/modules/log/logging.service.ts
@@ -8,7 +8,15 @@ import { CustomWritableStream } from './custom-writable-stream';
 export class LoggingService implements LoggerService {
   private readonly successLogger;
   private readonly errorLogger;
+  private readonly jsonLogger;
   private readonly logDirectory = path.join(process.cwd(), 'logs'); // Use process.cwd() instead of __dirname
+  private readonly logEntries: Array<{
+    timestamp: string;
+    level: string;
+    message: string;
+    context?: string;
+    stack?: string;
+  }> = []; // In-memory log storage
 
   constructor() {
     // Ensure log directory exists
@@ -19,10 +27,16 @@ export class LoggingService implements LoggerService {
       this.logDirectory,
       'application.log',
     );
+    const logsFilePath = path.join(this.logDirectory, 'error.json');
 
     const successStream = new CustomWritableStream(successLogFilePath);
     const errorStream = new CustomWritableStream(errorLogFilePath);
     const combinedStream = new CustomWritableStream(applicationLogFilePath);
+    const jsonStream = new CustomWritableStream(logsFilePath);
+
+    const successFormat = format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} ${level}: ${message}`;
+    });
 
     const errorFormat = format.printf(
       ({ timestamp, level, message, stack }) => {
@@ -34,8 +48,16 @@ export class LoggingService implements LoggerService {
       },
     );
 
-    const successFormat = format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} ${level}: ${message}`;
+    const jsonFormat = format.printf(({ timestamp, level, message, stack }) => {
+      const logMessage = {
+        timestamp,
+        level,
+        message,
+      };
+      if (stack) {
+        logMessage['stack'] = stack;
+      }
+      return JSON.stringify(logMessage);
     });
 
     const commonOptions = {
@@ -54,7 +76,7 @@ export class LoggingService implements LoggerService {
           format: format.combine(format.colorize(), successFormat),
         }),
         new transports.Stream({ stream: successStream }),
-        new transports.Stream({ stream: combinedStream }), // Also write to combined log
+        new transports.Stream({ stream: combinedStream }),
       ],
     });
 
@@ -70,10 +92,53 @@ export class LoggingService implements LoggerService {
         new transports.Stream({ stream: combinedStream }), // Also write to combined log
       ],
     });
+
+    this.jsonLogger = createLogger({
+      ...commonOptions,
+      level: 'error',
+      format: jsonFormat,
+      transports: [
+        new transports.Console({
+          format: format.combine(format.colorize(), jsonFormat),
+        }),
+        new transports.Stream({ stream: jsonStream }),
+      ],
+    });
+
+    // get error.json file ant set logEntries
+    this.jsonLogger.query({}, (err, results) => {
+      if (err) {
+        this.errorLogger.error('Error reading error.json file', { err });
+      } else {
+        this.logEntries = results.map((log) => ({
+          timestamp: log.timestamp,
+          level: log.level,
+          message: log.message,
+          context: log.context,
+          stack: log.stack,
+        }));
+      }
+    });
+  }
+
+  private addLogEntry(
+    level: string,
+    message: string,
+    context?: string,
+    stack?: string,
+  ) {
+    this.logEntries.push({
+      timestamp: new Date().toISOString(),
+      level,
+      message,
+      context,
+      stack,
+    });
   }
 
   log(message: string, context?: string) {
     this.successLogger.info(message, { context });
+    this.addLogEntry('info', message, context);
   }
 
   error(message: string | Error, trace?: string, context?: string) {
@@ -82,27 +147,50 @@ export class LoggingService implements LoggerService {
         stack: message.stack,
         context,
       });
+      this.jsonLogger.error(message.message, {
+        stack: message.stack,
+        context,
+      });
+      this.addLogEntry('error', message.message, context, message.stack);
     } else {
       this.errorLogger.error(message, {
         stack: trace,
         context,
       });
+      this.jsonLogger.error(message, {
+        stack: trace,
+        context,
+      });
+      this.addLogEntry('error', message, context, trace);
     }
   }
 
   warn(message: string, context?: string) {
     this.successLogger.warn(message, { context });
+    this.addLogEntry('warn', message, context);
   }
 
   debug(message: string, context?: string) {
     this.successLogger.debug(message, { context });
+    this.addLogEntry('debug', message, context);
   }
 
   verbose(message: string, context?: string) {
     this.successLogger.verbose(message, { context });
+    this.addLogEntry('verbose', message, context);
   }
 
   getLoggerFilePath(): string {
     return path.join(this.logDirectory, 'application.log');
+  }
+
+  getLogEntries(): Array<{
+    timestamp: string;
+    level: string;
+    message: string;
+    context?: string;
+    stack?: string;
+  }> {
+    return this.logEntries;
   }
 }
