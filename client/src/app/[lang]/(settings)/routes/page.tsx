e3f4e883dd6609a2681dca9032317171b655e905
nazmul53p <nazmul2018s@gmail.com>
2025-04-29 17:43:42 +0600

    dfghjk

--- client/src/app/[lang]/(settings)/routes/page.tsx
+++ client/src/app/[lang]/(settings)/routes/page.tsx
@@ -1,9 +1,11 @@
 import Tree from "@ui/settings/routes";
+import { cookies } from "next/headers";
 import { FC } from "react";
 
 export interface IVisualizeReactApp {}
 
-const VisualizeReactApp: FC<IVisualizeReactApp> = () => {
+const VisualizeReactApp: FC<IVisualizeReactApp> = async () => {
+    console.log((await cookies()).get("token")?.value);
     return (
         <main>
             <Tree />
