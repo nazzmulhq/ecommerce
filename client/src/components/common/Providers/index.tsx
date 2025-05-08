583e1578f93acc48f81a5f956c594ef515143d62
nazmul53p <nazmul2018s@gmail.com>
2025-05-09 01:00:31 +0600

    done

--- client/src/components/common/Providers/index.tsx
+++ client/src/components/common/Providers/index.tsx
@@ -2,7 +2,6 @@
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import AppContextProvider from "@lib/context/AppContextProvider";
 import AppLocaleProvider from "@lib/context/AppLocaleProvider";
-import StyledComponentsRegistry from "@lib/context/AppStyleProvider";
 import AppThemeProvider from "@lib/context/AppThemeProvider";
 import { GlobalStyles } from "@lib/theme/GlobalStyle";
 import "antd/dist/reset.css";
@@ -10,24 +9,25 @@ import { FC, ReactNode } from "react";
 import "simplebar-react/dist/simplebar.min.css";
 import "../../../../public/styles/index.css";
 import AppLayout from "../AppLayout";
+import StyledComponentsRegistry from "./StyledComponentsRegistry";
 export interface IProviders {
     children: ReactNode;
 }
 
 const Providers: FC<IProviders> = ({ children }) => {
     return (
-        <StyledComponentsRegistry>
-            <AntdRegistry>
-                <AppContextProvider>
-                    <AppThemeProvider>
-                        <AppLocaleProvider>
+        <AppContextProvider>
+            <AppThemeProvider>
+                <AppLocaleProvider>
+                    <AntdRegistry>
+                        <StyledComponentsRegistry>
                             <GlobalStyles />
                             <AppLayout>{children}</AppLayout>
-                        </AppLocaleProvider>
-                    </AppThemeProvider>
-                </AppContextProvider>
-            </AntdRegistry>
-        </StyledComponentsRegistry>
+                        </StyledComponentsRegistry>
+                    </AntdRegistry>
+                </AppLocaleProvider>
+            </AppThemeProvider>
+        </AppContextProvider>
     );
 };
 
