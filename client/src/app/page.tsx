2279b715f729b6e401cf99e855deb333e54784cf
nazmul53p <nazmul2018s@gmail.com>
2025-05-10 14:05:23 +0600

    cvb

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,26 +5,7 @@ import { FC } from "react";
 export interface IComponent {}
 
 const Component: FC<IComponent> = () => {
-    return (
-        <>
-            {[...Array(50)].map((_, index) => (
-                <div
-                    key={index}
-                    style={{
-                        height: "100px",
-                        backgroundColor: index % 2 === 0 ? "lightblue" : "lightgreen",
-                        margin: "5px",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        width: "50%",
-                    }}
-                >
-                    Item {index + 1}
-                </div>
-            ))}
-        </>
-    );
+    return <>Home Page</>;
 };
 
 export default Component;
